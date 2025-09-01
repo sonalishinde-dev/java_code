cfcdebe09bc0ac608129fcd4ecb9b12fbf5ffc9b
sonalishinde-dev <sonalishinde08123@gmail.com>
2025-09-01 11:54:50 +0530

    java start

---
+++ First.java
@@ -0,0 +1,11 @@
+package first;
+
+public class First {
+
+	public static void main(String[] args) {
+
+	System.out.println("hello world!");
+	
+	}
+
+}
---
+++ Second.java
@@ -0,0 +1,25 @@
+package first;
+
+public class Second {
+
+	public static void main(String[] args) {
+		
+		int age=28;
+		
+		float weight=47.4f;
+		
+		double salary=23789.23;
+		
+		char gender='m';
+		
+		String name="fthtjhfhj";
+		
+		System.out.println(age);
+		
+		System.out.println(age+weight+salary);
+		
+		System.out.println(age+weight+gender+name+salary);
+
+	}
+
+}
