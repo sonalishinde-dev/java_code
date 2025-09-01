42d8b887e78ab51cd3c504a2f91de8f5b21784d7
sonalishinde-dev <sonalishinde08123@gmail.com>
2025-09-01 12:27:23 +0530

    add the new variable

---
+++ Third.java
@@ -0,0 +1,26 @@
+package first;
+
+public class Third {
+
+	public static void main(String[] args) {
+		
+		int age=35;
+		
+		int wight=79;
+		
+		char name='s';
+		
+		String msg="hgujkujoik";
+		
+		System.out.println(age+wight+msg);
+		
+		System.out.println(age+msg+name);
+		
+		System.out.println(name+age+wight);
+		
+		
+		
+
+	}
+
+}
