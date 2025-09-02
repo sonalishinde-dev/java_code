33de90a137cc837307d617e039e4f25cf1dea0c0
sonalishinde-dev <sonalishinde08123@gmail.com>
2025-09-02 13:19:48 +0530

    write a array code

---
+++ Array.java
@@ -0,0 +1,16 @@
+package first;
+
+public class Array {
+
+	public static void main(String[] args) {
+
+		int[]arr=new int[10];
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(arr[2]);
+		}
+				
+	}
+
+}
