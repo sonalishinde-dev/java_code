6f73f921ba3c78a9afaa1399ea65ba23a3890c28
sonalishinde-dev <sonalishinde08123@gmail.com>
2025-09-02 12:53:18 +0530

    code type for typecasting

---
+++ Typecastig1.java
@@ -0,0 +1,28 @@
+package first;
+
+public class Typecastig1 {
+
+	public static void main(String[] args) {
+		
+		 
+		char b='y';
+		
+		double s=(double)b;
+		
+		System.out.println(s);
+		
+		char a='d';
+		
+		float k=(float)a;
+		
+		System.out.println(k);
+		
+		float d=787912.24f;
+		
+		char e=(char)d;
+		
+		System.out.println(e);
+		
+	}
+
+}
