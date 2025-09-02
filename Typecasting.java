671445a59ef18f9bca4d16131a89ae256f6cf980
sonalishinde-dev <sonalishinde08123@gmail.com>
2025-09-02 12:07:35 +0530

    for type casting

---
+++ Typecasting.java
@@ -0,0 +1,38 @@
+package first;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+
+		double i=234.234;
+		
+		int d=(int)i;
+		
+		System.out.println(d);
+		
+		int a=2325;
+		
+		double b=(double)a;
+		
+		System.out.println(b);
+		
+		int x=67;
+		
+		char y=(char)x;
+		
+		System.out.println(y);
+		
+		char n='s';
+		
+		int m=(char)n;
+		
+		System.out.println(m);
+		
+		String r="hwgh";
+		
+		
+		
+		
+	}
+
+}
