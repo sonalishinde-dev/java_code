8ee0ab3c37ac5e1423f94b71b1ef8f7f02e9a540
sonalishinde-dev <sonalishinde08123@gmail.com>
2025-09-08 11:49:24 +0530

    assing op

---
+++ Assingmentop.java
@@ -0,0 +1,22 @@
+package first;
+
+public class Assingmentop {
+
+	public static void main(String[] args) {
+		
+		int a=10;    
+		a+=3;//10+3    
+		System.out.println(a);    
+		a-=4;//13-4    
+		System.out.println(a);    
+		a*=2;//9*2    
+		System.out.println(a);    
+		a/=2;//18/2    
+		System.out.println(a);    
+		a %= 3; // 9 % 3 = 0  
+		System.out.println(a);    
+
+		System.out.println(a);
+	}
+
+}
---
+++ Concatination.java
@@ -0,0 +1,19 @@
+package first;
+
+public class Concatination {
+
+	public static void main(String[] args) {
+		
+		String a="sonali";
+		
+		String b="shinde";
+		
+		int age=28;
+		
+		double wight=47d;
+		
+		System.out.println("My name is: "+a+ " "+b+" "+ "& age is"+" " +age +" "+  "my wight is"+" "+wight );
+
+	}
+
+}
